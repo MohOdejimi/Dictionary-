c32f66077ac83f4b9a7cfdec11843a430d27f2ce
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-04-16 12:00:54 +0100

    New Edits

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-document.querySelector('button').addEventListener('click', getfn)
+document.querySelector('.search').addEventListener('click', getfn)
+const sound = document.querySelector('#sound')
+sound.addEventListener('click', playSound)
 function getfn() {
-    let input = document.querySelector('input').value
+    let input = document.querySelector('.text').value
 
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
     .then(res => res.json())
     .then(data => {
      console.log(data)
-    document.querySelector('.word').innerText = `${input}`
+    document.querySelector('.word h3').innerText = `${input}`
     document.querySelector('.partOfSpeech').innerText = (data[0].meanings[0].partOfSpeech).toLowerCase()
     document.querySelector('.transcription').innerText = data[0].phonetic
-    document.querySelector('.one').innerText = data[0].meanings[0].definitions[0].definition
-    document.querySelector('.two').innerText = data[0].meanings[1].definitions[0].definition
+    document.querySelector('.definition').innerText = data[0].meanings[0].definitions[0].definition
+    document.querySelector('.example').innerText = data[0].meanings[0].definitions[0].example
+    sound.setAttribute("src", `${data[0].phonetics[0].audio}`)
+    console.log(sound)
    })
 }
-
+function playSound() {
+  sound.play();
+}
