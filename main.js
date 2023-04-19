4be862e09fc2acf9bbc28d2c3ca593e11d665669
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-04-19 18:46:09 +0100

    Functions edits

--- main.js
+++ main.js
@@ -13,8 +13,18 @@ function getfn() {
     document.querySelector('.definition').innerText = data[0].meanings[0].definitions[0].definition
     document.querySelector('.example').innerText = data[0].meanings[0].definitions[0].example
     sound.setAttribute("src", data[0].phonetics[0].audio)
-    console.log(sound)
+    let meanings;
+       data.forEach(obj => {
+      meanings = Object.entries(obj.meanings)
+    })
+    /*meanings.forEach(meaning => Object.entries(meaning.definition))*/
+    console.log(meanings.forEach(meaning => Object.entries(meaning)))
    })
+   .catch(err => {
+     document.querySelector('.example').innerText = `You can try the search again at later time or head to the web instead.`
+     document.querySelector('.definition').innerText = `Sorry pal, we couldn't find definitions for the word you were looking for`
+   })
+   
 }
 function playSound() {
   sound.play();
